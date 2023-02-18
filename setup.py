d65a901d927dbc5bc6e357d15ebe0a9a82606f58
Dude <>
2023-02-18 23:59:58 +0300

    wip

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 setup(
     name = 'my-daemon',
     version = '0.0.0',
-    # packages = ['main'],
+    packages = ['daemon'],
     entry_points = {
         'console_scripts': [
-            'my-daemon = main.__main__:main'
+            'my-daemon = daemon.daemon:main'
         ]
 })
