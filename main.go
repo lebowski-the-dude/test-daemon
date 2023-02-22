e7ee1551de55f8ce5b3d709f60107761d635694f
Dude <>
2023-02-22 23:14:15 +0300

    wip

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func testFunction() {
 func getOutput() string {
     out, err := exec.Command("xrandr").Output()
     if err != nil {
-		log.Printf(err)
+		log.Print(err)
         log.Fatal(err)
     }
     outString := fmt.Sprintf("%s", out)
