524d8564b7a1bc83cb419633949ae24860acb49a
Dude <>
2023-02-22 17:47:40 +0300

    wip

---
+++ go.mod
@@ -0,0 +1,9 @@
+module test
+
+go 1.20
+
+require (
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/sevlyar/go-daemon v0.1.6 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "fmt"
+    "log"
+	"time"
+    "os/exec"
+
+	"github.com/sevlyar/go-daemon"
+)
+
+func main() {
+	cntxt := &daemon.Context{
+		PidFileName: "/var/run/sample.pid",
+		PidFilePerm: 0644,
+		LogFileName: "/var/log/sample.log",
+		LogFilePerm: 0640,
+		WorkDir:     "/",
+		Umask:       027,
+		Args:        []string{"[go-daemon sample]"},
+	}
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		log.Fatal("Unable to run: ", err)
+	}
+	if d != nil {
+		return
+	}
+	defer cntxt.Release()
+
+	log.Print("- - - - - - - - - - - - - - -")
+	log.Print("daemon started")
+
+	testFunction()
+}
+
+func testFunction() {
+	for {
+		log.Print(getOutput())
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func getOutput() string {
+    out, err := exec.Command("xrandr").Output()
+    if err != nil {
+        log.Fatal(err)
+    }
+    outString := fmt.Sprintf("%s", out)
+
+	return outString
+}
